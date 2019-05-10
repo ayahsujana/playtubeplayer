6207e3e5b30c5b879959ef4ffa596a15d4f1ab4e
Dedy Sujana <dedysujana@gmail.com>
2019-05-10 08:58:10 +0700

    update some code

--- lib/api/google_key.dart
+++ lib/api/google_key.dart
@@ -1,5 +1,5 @@
 
-const String apiKey = 'AIzaSyAvywoHr2s3I9zlEwBbiCs3D-zsMr3-46Y';
+const String apiKey = 'your api youtube';
 const String app_id = '';
 const String banner = '';
 const String inters = '';
